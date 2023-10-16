c9452b8c6708ca31f6f532dc9144a6e8a18e6930
Mayordev <mayowadavid2000@gmail.com>
2023-10-16 18:06:04 +0100

    second commit

--- app/src/main/java/com/example/platenumber/MainActivity.kt
+++ app/src/main/java/com/example/platenumber/MainActivity.kt
@@ -6,38 +6,51 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.util.Log
+import com.example.platenumber.RetrofitHelper.createRetrofitInstance
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+    val edit : EditText = findViewById(R.id.editPlate)
+    val plateNumber = edit.text.toString()
+    val submit: Button = findViewById(R.id.button)
+    lateinit var vehicleStatusApiService: VehicleStatusApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+         vehicleStatusApiService = createRetrofitInstance().create(VehicleStatusApiService::class.java)
+        // launching a new coroutine
 
-        val submit: Button = findViewById(R.id.button)
-
-        val vehicleStatusApiService = RetrofitHelper.getInstance().create(VehicleStatusApiService::class.java)
 
         submit.setOnClickListener {
             verify()
-
         }
 
     }
 
-    fun verify (){
-        val edit : EditText = findViewById(R.id.editPlate)
-        val plateNumber = edit.text.toString()
+     fun verify (){
         if (plateNumber.length != 10){
             Toast.makeText(this, "Please enter a valid Plate Number", Toast.LENGTH_LONG).show()
         }
         else{
-            val intent = Intent(this, MainActivity2::class.java).also{
-                it.putExtra("EXTRA_MESSAGE", plateNumber)
-                startActivity(it)
+            GlobalScope.launch {
+                val result = vehicleStatusApiService.getVehicleStatus(plateNumber).execute()
+                if (result != null) {
+                    val intent = Intent(this@MainActivity, MainActivity2::class.java).also {
+                        it.putExtra("EXTRA_MESSAGE", plateNumber)
+                        startActivity(it)
+                    }
+                    // Checking the results
+                    Log.d("mayor: ", result.body().toString())
+                }
             }
+
+
+
         }
 
     }
